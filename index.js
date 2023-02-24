76227bebf7d317c430aa9f0bca1076d52656331d
OlegKorniychuk <kornoleg576@gmail.com>
2023-02-24 15:48:22 +0200

    Implemented app start in both modes. App fully functional

--- index.js
+++ index.js
@@ -50,3 +50,34 @@ const getArgsFromFile = (path) => {
   }
 }
 
+const startApp = () => {
+  const path = process.argv[2];
+  if (path) {
+    const args = getArgsFromFile(path);
+    displayEquation(...args);
+    solveQuadEquation(...args);
+  } else {
+    const params = ['a', 'b', 'c'];
+    const values = [];
+    console.log('Input parameters` values: ');
+    process.stdout.write(params[0]+'= ');
+    process.stdin.on('data', data => {
+      if (values.length < 3) {
+        if (data.length != 0 && parseFloat(data)) {
+          values.push(data);
+          if (values.length < 3) {
+            process.stdout.write(params[values.length]+'= ');
+          } else {
+            displayEquation(...values);
+            solveQuadEquation(...values);
+          }
+        } else {
+          process.stdout.write('Parameter value must be a number');
+          process.stdout.write('\n' + params[values.length] + '= ');
+        }
+      }
+    })
+  }
+}
+
+startApp();
